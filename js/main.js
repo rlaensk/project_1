c32c5f9db69118d4a12c709a7c78ed030477cfb4
김두나 <110874398+rlaensk@users.noreply.github.com>
2022-11-29 15:33:50 +0900

    2022.11.29-04

---
+++ js/main.js
@@ -0,0 +1,29 @@
+$(function () {
+    $(".bs5").hover(function () {
+        $(".bs1").show();
+        $(".bs2").hide();
+        $(".bs3").hide();
+        $(".bs4").hide();
+    });
+    $(".bs6").hover(function () {
+        $(".bs2").show();
+        $(".bs1").hide();
+        $(".bs3").hide();
+        $(".bs4").hide();
+    });
+    $(".bs7").hover(function () {
+        $(".bs3").show();
+        $(".bs2").hide();
+        $(".bs1").hide();
+        $(".bs4").hide();
+
+    });
+    $(".bs8").hover(function () {
+        $(".bs4").show();
+        $(".bs2").hide();
+        $(".bs3").hide();
+        $(".bs1").hide();
+    });
+
+
+});
